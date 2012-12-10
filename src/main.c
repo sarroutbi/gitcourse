e71c7e9873692a41a339ceaac372780a199522da
esergbr <sergio.arroutbi.braojos@ericsson.com>
2012-12-10 12:29:02 +0000

    Changing src file on master branch

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-  printf("Hello world\n");
+  printf("Hello world, dear world\n");
   printf("Hello, another world\n");
   printf("Hello, last world\n");
   printf("Hello, I am changing master branch\n");
